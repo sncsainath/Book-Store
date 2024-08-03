40635979898690e41d4ebee0c3931138bfcf2a36
sncsainath <sncsainath@gmail.com>
2024-08-03 15:56:06 +0530

    The more I practice the more i can learn

--- 03-frontend/react-library/src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -10,7 +10,7 @@ export const ExploreTopBooks = () => {
                         <h1 className='display-5 fw-bold'>Find your next adventure</h1>
                         <p className='col-md-8 fs-4'>Where would you like to go next ? </p>
                         <Link type='button' className='btn main-color btn-lg text-white' to="/search">Explore Top Books</Link>
-                        <p>This is the line which i am adding </p>
+                        <p>The man with massess </p>
                     </div>
             </div>
 
